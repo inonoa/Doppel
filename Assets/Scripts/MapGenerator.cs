b94d85f8b6cd71e624e99ede1d01a339edc0c60e
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-08-11 23:15:33 +0900

    プロジェクト作った

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
