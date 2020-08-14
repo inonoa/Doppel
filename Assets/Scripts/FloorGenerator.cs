707ca806ad3ade0f666f4325e4a79efe3b09c478
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-08-15 00:56:20 +0900

    FloorGenerator

---
+++ Assets/Scripts/FloorGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using Sirenix.OdinInspector;
+
+public class FloorGenerator : SerializedMonoBehaviour
+{
+    GeneratedMap map;
+
+    [SerializeField] MapGenerator generator;
+    [SerializeField] IMapView view;
+
+    void Start()
+    {
+        map = generator.Generate();
+        view.SetMap(map);
+        Debug.Log($"主人公: {RandomFloorTile(map)}");
+    }
+
+    Vector2Int RandomFloorTile(GeneratedMap map)
+    {
+        int height = map.Tiles.Count;
+        int width = map.Tiles[0].Count;
+
+        Vector2Int ans;
+        while(true)
+        {
+            ans = new Vector2Int(Random.Range(0, width - 1), Random.Range(0, height - 1));
+            if(map.Tiles[ans.y][ans.x] == TileType.Floor) break;
+        }
+        return ans;
+    }
+}
+
+public interface IMapView
+{
+    void SetMap(GeneratedMap map);
+}
---
+++ Assets/Scripts/MapDebugView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using Sirenix.OdinInspector;
+
+public class MapDebugView : MonoBehaviour, IMapView
+{
+    [SerializeField] Text text;
+
+    public void SetMap(GeneratedMap map)
+    {
+        string txt = "";
+
+        for(int i = 0; i < map.Tiles[0].Count; i++)
+        {
+            txt += i.ToString("00");
+        }
+        txt += "\n";
+
+        for(int i = 0; i < map.Tiles.Count; i++)
+        {
+            txt += i.ToString("00");
+            foreach(TileType tile in map.Tiles[i])
+            {
+                txt += tile.ToDebugChar() + " ";
+            }
+            txt += "\n";
+        }
+
+        text.text = txt;
+    }
+}
