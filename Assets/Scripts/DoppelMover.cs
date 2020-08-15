1ee493b8efecb5b2fe6080662da24c420bfbc7eb
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-08-15 17:50:48 +0900

    改善した

--- Assets/Scripts/DoppelMover.cs
+++ Assets/Scripts/DoppelMover.cs
@@ -49,42 +49,50 @@ public class DoppelMover : MonoBehaviour, IUnderTurns
 
     Vector2Int NextPos()
     {
-        //時計回りに二周探索
-        //一周目、通路があればそこ
-        for(int i = (int) lastDir; i < ((int) lastDir) + 4; i ++)
-        {
-            Vector2Int forward = Forward((Dir)(i % 4));
-            if(status.hero.PosOnMap == forward) continue;
-            if(status.doppels.Any(dp => forward == dp.PosOnMap)) continue;
-            if(status.map.GetTile(forward) == TileType.Wall) continue;
-
-            if(status.map.GetTile(forward) == TileType.Aisle)
-            {
-                lastDir = (Dir)(i % 4);
-                return forward;
-            }
-        }
-        //二周目、部屋内の床があればそこ
-        for(int i = (int) lastDir; i < ((int) lastDir) + 4; i ++)
-        {
-            Vector2Int forward = Forward((Dir)(i % 4));
-            if(status.hero.PosOnMap == forward) continue;
-            if(status.doppels.Any(dp => forward == dp.PosOnMap)) continue;
-            if(status.map.GetTile(forward) == TileType.Wall) continue;
-            
-            if(status.map.GetTile(forward) == TileType.RoomFloor
-            || status.map.GetTile(forward) == TileType.Stair)
-            {
-                lastDir = (Dir)(i % 4);
-                return forward;
-            }
-        }
+        //優先順位: 正面の通路>横の通路>正面の部屋床>横の部屋床>後ろの通路>後ろの部屋床
         //どこにも行けなければ足踏み
+        Dir[] dirs = DirPriority(lastDir);
+        Vector2Int[] priority = DirPriority(lastDir).Select(Forward).ToArray();
+
+        if(CanMove(priority[0]) &&  status.map.GetTile(priority[0]) == TileType.Aisle ){ lastDir = dirs[0]; return priority[0]; }
+        if(CanMove(priority[1]) &&  status.map.GetTile(priority[1]) == TileType.Aisle ){ lastDir = dirs[1]; return priority[1]; }
+        if(CanMove(priority[2]) &&  status.map.GetTile(priority[2]) == TileType.Aisle ){ lastDir = dirs[2]; return priority[2]; }
+        if(CanMove(priority[0]) && (status.map.GetTile(priority[0]) == TileType.RoomFloor
+                                ||  status.map.GetTile(priority[0]) == TileType.Stair)){ lastDir = dirs[0]; return priority[0]; }
+        if(CanMove(priority[1]) && (status.map.GetTile(priority[1]) == TileType.RoomFloor
+                                ||  status.map.GetTile(priority[1]) == TileType.Stair)){ lastDir = dirs[1]; return priority[1]; }
+        if(CanMove(priority[2]) && (status.map.GetTile(priority[2]) == TileType.RoomFloor
+                                ||  status.map.GetTile(priority[2]) == TileType.Stair)){ lastDir = dirs[2]; return priority[2]; }
+        if(CanMove(priority[3]) &&  status.map.GetTile(priority[3]) == TileType.Aisle ){ lastDir = dirs[3]; return priority[3]; }
+        if(CanMove(priority[3]) && (status.map.GetTile(priority[3]) == TileType.RoomFloor
+                                ||  status.map.GetTile(priority[3]) == TileType.Stair)){ lastDir = dirs[3]; return priority[3]; }
+
         return PosOnMap;
+    }
+
+    bool CanMove(Vector2Int nextPos)
+    {
+        if(status.hero.PosOnMap == nextPos) return false;
+        if(status.doppels.Any(dp => nextPos == dp.PosOnMap)) return false;
+        if(status.map.GetTile(nextPos) == TileType.Wall) return false;
+        
+        return true;
+    }
+
+    Dir[] DirPriority(Dir first)
+    {
+        Dir[] ans = new Dir[4];
+        ans[0] = first; //正面
+        ans[1] = (Dir) (((int) first + 1) % 4); //自分から見て左
+        ans[2] = (Dir) (((int) first + 3) % 4); //自分から見て右
+        ans[3] = (Dir) (((int) first + 2) % 4); //自分から見て後ろ
+
+        return ans;
     }
 
     Vector2Int Forward(Dir dir)
     {
+        Debug.Log(dir);
         switch(dir)
         {
         case Dir.R: return PosOnMap + new Vector2Int(1, 0);
--- Assets/Scripts/GeneratedMap.cs
+++ Assets/Scripts/GeneratedMap.cs
@@ -10,7 +10,9 @@ public class GeneratedMap
     public IReadOnlyList<IReadOnlyList<TileType>> Tiles => _Tiles;
     public int Width => Tiles[0].Count;
     public int Height => Tiles.Count;
-    public TileType GetTile(Vector2Int pos) => Tiles[pos.y][pos.x];
+    public TileType GetTile(Vector2Int pos){
+        return Tiles[pos.y][pos.x];
+    }
 
     public GeneratedMap(MapInTheMaking madeMap)
     {
