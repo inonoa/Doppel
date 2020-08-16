6e32a4432738e9af70449f57a26e91cbae7c19b9
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-08-16 23:31:45 +0900

    視界を黒くする

--- Assets/Scripts/MapView.cs
+++ Assets/Scripts/MapView.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class MapView : MonoBehaviour, IMapView
 {
@@ -10,12 +11,16 @@ public class MapView : MonoBehaviour, IMapView
     [SerializeField] float floorY = -0.5f;
     [SerializeField] float _TileSize = 1;
     public float TileSize => _TileSize;
+    [SerializeField] Color herVviewColor = new Color(0.2f, 0.2f, 0.2f, 1);
 
     FloorStatus status;
+
+    MeshRenderer[][] floorRenderers;
 
     public void SetStatus(FloorStatus status)
     {
         this.status = status;
+        floorRenderers = new MeshRenderer[status.map.Height][];
 
         Vector3 upPosition = new Vector3(0, 0, (status.map.Height - 1) / 2.0f * _TileSize);
         for(int i = 0; i < status.map.Height; i ++)
@@ -23,6 +28,8 @@ public class MapView : MonoBehaviour, IMapView
             GameObject row = new GameObject("Row" + i);
             row.transform.SetParent(this.transform);
             row.transform.position = upPosition - i * new Vector3(0, 0, _TileSize);
+
+            floorRenderers[i] = new MeshRenderer[status.map.Width];
 
             Vector3 leftPosition = new Vector3(- (status.map.Width - 1) / 2.0f * _TileSize, 0, 0);
             for(int j = 0; j < status.map.Width; j ++)
@@ -35,6 +42,28 @@ public class MapView : MonoBehaviour, IMapView
 
                 MeshFilter floor = Instantiate(floorPrefab, row.transform);
                 floor.transform.localPosition = new Vector3(0, floorY, 0) + leftPosition + j * new Vector3(_TileSize, 0, 0);
+                floorRenderers[i][j] = floor.GetComponent<MeshRenderer>();
+            }
+        }
+    }
+
+    void Update()
+    {
+        Vector2Int[] view = status.hero.GetView()
+                            .Append(status.hero.PosOnMap)
+                            .ToArray();
+        foreach(int i in Enumerable.Range(0, floorRenderers.Length))
+        {
+            foreach(int j in Enumerable.Range(0, floorRenderers[i].Length))
+            {
+                if(view.Contains(new Vector2Int(j, i)))
+                {
+                    floorRenderers[i][j].material.color = herVviewColor;
+                }
+                else
+                {
+                    floorRenderers[i][j].material.color = new Color(1, 1, 1, 1);
+                }
             }
         }
     }
